d1672096af5a2a0750bf7f07d6faf54970622767
Rangosolo1234 <kipkirui.solomon864@gmail.com>
2025-09-07 12:49:23 +0300

    Creating a class in python

---
+++ classes_objects/createClass.py
@@ -0,0 +1,5 @@
+'''Classes are created using class keyword. Attributes are variables defined inside class and represent properties of the class.
+=> Attributes can be accessed using dot . operator(eg MyClass.my_attribute)'''  
+#Define a class
+class Dog:
+    sound = "bark"
